7f745ac88b1c3d0ce7f013190941d615bee0b5c1
elliowo <7861226+elliowo@users.noreply.github.com>
2025-03-07 09:59:17 +0000

    emacs font

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -53,11 +53,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(company helm dape tree-sitter-langs tree-sitter zig-mode rustic projectile page-break-lines avy ido-vertical-mode which-key nov pdf-tools doom-themes doom-modeline all-the-icons nerd-icons dashboard magit dimmer embark-consult embark consult marginalia vertico org-modern)))
+ '(package-selected-packages '(auctex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#282828" :foreground "#ebdbb2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 105 :width normal :foundry "CTDB" :family "FiraCode Nerd Font Mono")))))
+ )
